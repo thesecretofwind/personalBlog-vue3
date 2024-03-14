ede866713f2e25ac520c11581d7213d60d7d5748
chengguang.li <Chengguang.Li@vertiv.com>
2024-03-14 18:04:57 +0800

    fix：修改路由配置

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,38 +7,77 @@ import EditBlog from '@/pages/EditBlog.vue';
 import ShowTags from '@/pages/ShowTag.vue';
 import SingleBlog from '@/pages/SingleBlog.vue';
 import SingleTag from '@/pages/SingleTag.vue';
+import BlogList from "@/components/BlogList.vue";
 
 
 
 const routes: Array<RouteRecordRaw> = [
     {
         path: '/',
-        component: Home
+        component: Home,
+        redirect: '/overview',
+        children: [
+            {
+                path: '/overview',
+                name: 'overview',
+                component: BlogList
+            },
+            {
+                path: '/add',
+                name: 'AddBlog',
+                component: AddBlog
+            },
+            {
+                path: '/abulm',
+                name: 'Abulm',
+                component: Abulm
+            },
+            {
+                path: '/edit/:id',
+                name: 'EditBlog',
+                component: EditBlog
+            },
+            {
+                path: '/tags',
+                name: 'ShowTags',
+                component: ShowTags
+            },
+            {
+                path: '/blog/:id',
+                name: 'SingleBlog',
+                component: SingleBlog
+            },
+            {
+                path: '/tag/:id',
+                name: 'SingleTag',
+                component: SingleTag
+            }
+        ]
     },
-    {
-        path: '/add',
-        component: AddBlog
-    },
-    {
-        path: '/blog/:id',
-        component: SingleBlog
-    },
-    {
-        path: '/edit/:id',
-        component: EditBlog,
-    },
-    {
-        path: '/tag/:id',
-        component: ShowTags
-    },
-    {
-        path: '/singleTag/:id',
-        component: SingleTag
-    },
-    {
-        path: '/ablum',
-        component: Abulm
-    }
+    // {
+    //     path: '/add',
+    //     component: AddBlog
+    // },
+    // {
+    //     path: '/blog/:id',
+    //     component: SingleBlog
+    // },
+    // {
+    //     path: '/edit/:id',
+    //     component: EditBlog,
+    // },
+    // {
+    //     path: '/tag/:id',
+    //     component: ShowTags
+    // },
+    // {
+    //     path: '/singleTag/:id',
+    //     component: SingleTag
+    // },
+    // {
+    //     path: '/ablum',
+    //     component: Abulm
+    // }
 ];
 
 const router = createRouter({
