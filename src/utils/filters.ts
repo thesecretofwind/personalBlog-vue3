1e9050f3c3aab9cd2364955f38f983a28a100e9b
chengguang.li <Chengguang.Li@vertiv.com>
2024-03-14 18:05:21 +0800

    feat：抽离过滤器作为utils工具类

---
+++ src/utils/filters.ts
@@ -0,0 +1,11 @@
+export function toUpperCase(value: string) {
+    return value.toUpperCase();
+}
+
+export function snippet(value: string) {
+    return value.slice(0, 100) + '....';
+};
+
+export function otherSinpper(value: string) {
+    return value.slice(0,20) + '...';
+}
